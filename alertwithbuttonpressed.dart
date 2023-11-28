e6958317a1ae61725190fb01a7cb33206d8aaa9a
Mithran-04 <103376467+Mithran-04@users.noreply.github.com>
2023-11-28 18:19:29 +0530

    Add files via upload

---
+++ alertwithbuttonpressed.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alert App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _controller = TextEditingController();
+
+  void _showAlert(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Alert'),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Alert App'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(labelText: 'Enter Text'),
+            ),
+            ElevatedButton(
+              onPressed: () => _showAlert('Button Pressed'),
+              child: Text('Show Alert on Button Press'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _showAlert(_controller.text),
+              child: Text('Show Alert with Text Input'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
