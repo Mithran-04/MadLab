734c164290bc70ad9e903f59bbb69d1420e4d9ee
Mithran-04 <103376467+Mithran-04@users.noreply.github.com>
2023-11-29 00:52:05 +0530

    Update NotificationReminder.dart

--- NotificationReminder.dart
+++ NotificationReminder.dart
@@ -1,82 +1,135 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:intl/intl.dart';
 
-import 'package:smaple_projectt/NotificationService.dart';
+void main() => runApp(MyApp());
 
-DateTime scheduleTime = DateTime.now();
-
-class NotificationHomePage extends StatefulWidget {
-
-
+class MyApp extends StatelessWidget {
   @override
-  State<NotificationHomePage> createState() => _NotificationHomePageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
 }
 
-class _NotificationHomePageState extends State<NotificationHomePage> {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+  DateTime? _selectedDateTime;
+
+  @override
+  void initState() {
+    super.initState();
+    initNotifications();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello"),
+        title: Text('DateTime Picker & Notification Example'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            DatePickerTxt(),
-            ScheduleBtn(),
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () async {
+                final DateTime? pickedDate = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2101),
+                );
+
+                if (pickedDate != null) {
+                  final TimeOfDay? pickedTime = await showTimePicker(
+                    context: context,
+                    initialTime: TimeOfDay.now(),
+                  );
+
+                  if (pickedTime != null) {
+                    setState(() {
+                      _selectedDateTime = DateTime(
+                        pickedDate.year,
+                        pickedDate.month,
+                        pickedDate.day,
+                        pickedTime.hour,
+                        pickedTime.minute,
+                      );
+                    });
+                  }
+                }
+              },
+              child: Text('Pick Date and Time'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                if (_selectedDateTime != null) {
+                  scheduleNotification();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Notification scheduled for $_selectedDateTime'),
+                    ),
+                  );
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Please pick a date and time first.'),
+                    ),
+                  );
+                }
+              },
+              child: Text('Schedule Notification'),
+            ),
           ],
         ),
       ),
     );
   }
-}
 
-class DatePickerTxt extends StatefulWidget {
-  const DatePickerTxt({
-    Key? key,
-  }) : super(key: key);
+  Future<void> scheduleNotification() async {
+    if (_selectedDateTime != null) {
+      final AndroidNotificationDetails androidPlatformChannelSpecifics =
+      AndroidNotificationDetails(
+        'your_channel_id',
+        'Your Channel Name',
 
-  @override
-  State<DatePickerTxt> createState() => _DatePickerTxtState();
-}
+        importance: Importance.max,
+        priority: Priority.high,
+      );
+      final NotificationDetails platformChannelSpecifics =
+      NotificationDetails(android: androidPlatformChannelSpecifics);
 
-class _DatePickerTxtState extends State<DatePickerTxt> {
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () {
-        DatePicker.showDateTimePicker(
-          context,
-          showTitleActions: true,
-          onChanged: (date) => scheduleTime = date,
-          onConfirm: (date) {},
-        );
-      },
-      child: const Text(
-        'Select Date Time',
-        style: TextStyle(color: Colors.blue),
-      ),
-    );
+      await flutterLocalNotificationsPlugin.schedule(
+        0,
+        'Scheduled Notification',
+        'This is a scheduled local notification',
+        _selectedDateTime!,
+        platformChannelSpecifics,
+        payload: 'item x',
+      );
+    }
   }
-}
 
-class ScheduleBtn extends StatelessWidget {
-  const ScheduleBtn({
-    Key? key,
-  }) : super(key: key);
+  Future<void> initNotifications() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('@mipmap/ic_launcher');
 
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      child: const Text('Schedule notifications'),
-      onPressed: () {
-        debugPrint('Notification Scheduled for $scheduleTime');
-        NotificationService().scheduleNotification(
-            title: 'Scheduled Notification',
-            body: '$scheduleTime',
-            scheduledNotificationDateTime: scheduleTime);
-      },
+    final InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+
+    );
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
     );
   }
 }
